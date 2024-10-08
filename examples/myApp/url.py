4f065af12e5d7d6a72971fea85ead1977deb698f
sahan <101819553+omerfshan@users.noreply.github.com>
2024-10-08 19:03:02 +0300

    add

--- examples/myApp/url.py
+++ examples/myApp/url.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('',views.index,name="index"),
     path('index',views.index,name="index"),
     path('details',views.details,name="details"),
-    path('list',views.list,name="list"),
+    path('lists',views.lists,name="list"),
     # str intleri str olarak alıyor bu yüzden sıra çok önemli
-    path('<int:category>',views.getByCategoryId),
+    path('<int:category_id>',views.getByCategoryId),
     path('<str:category>',views.getByCategory)
    
 ]
--- examples/myApp/views.py
+++ examples/myApp/views.py
@@ -1,26 +1,31 @@
-from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.shortcuts import render,redirect
+from django.http.response import HttpResponse,HttpResponseNotFound
 
+data={
+    "telefon":"telefon ürün",
+    "bilgisayar":"bilgisayar ürün",
+    "elektronik":"elektronik ürün"
 
+}
 def index(request):
     return  HttpResponse("merhaba")
 
 def details(request):
     return HttpResponse("details")
 
-def list(request):
-    return HttpResponse("list")
+def lists(request):
+    return HttpResponse("lists")
 
-def getByCategoryId(request,category):
-    return HttpResponse(category)
+def getByCategoryId(request,category_id):
+    category_list=list(data.keys())
+    if category_id>len(category_list):
+        return HttpResponseNotFound("yanlış kategori secimi")
+    redirect_text=category_list[category_id-1]
+    return redirect("/products/"+redirect_text)
 
 def getByCategory(request,category):
-    category_text=None
-    if category=="bilgisayar":
-        category_text="bilgisayar ürünleri"
-    elif category=="telefon":
-        category_text="telefon ürünlerii"
-    else:
-        category_text="yanlış kategori"
-    return HttpResponse(category_text)
-
+    try:
+      category_text=data[category]
+      return HttpResponse(category_text)
+    except:
+        return HttpResponseNotFound("yanlış kategori secimi")
