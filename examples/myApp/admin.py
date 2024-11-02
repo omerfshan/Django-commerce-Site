0dc43dd7000d2d0d74e150e3713ffa259713444a
sahan <101819553+omerfshan@users.noreply.github.com>
2024-11-02 05:51:41 +0300

    add

--- examples/myApp/admin.py
+++ examples/myApp/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product,Category
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display=("name","price","isActivate","slug",)
+    list_display=("name","price","isActivate","slug","selectedCategories",)
     list_display_links=("name","price",)
-    readonly_fields=("slug",)
+    prepopulated_fields={"slug":("name",)}
     list_filter=("price","category",)
     list_editable=("isActivate",)
     search_fields=("name","description")
 
+    def selectedCategories(self,obj):
+      html=""
+      
+      for category in obj.category.all():
+       html += category.name + " "
+
+      return html
+    
 
 admin.site.register(Product,ProductAdmin)
+admin.site.register(Category)
--- examples/myApp/models.py
+++ examples/myApp/models.py
@@ -1,18 +1,25 @@
 from django.db import models
 from django.utils.text import slugify
 # Create your models here.
+
+class Category(models.Model):
+   name=models.CharField(max_length=100)
+   
+   def __str__(self):
+      return self.name
+  
 class Product(models.Model):
   name=models.CharField(max_length=50)
   price=models.DecimalField(max_digits=8,decimal_places=2)
   description=models.CharField(max_length=200)
   imageUrl=models.CharField(max_length=50)
   isActivate=models.BooleanField(default=False)
-  category=models.CharField(max_length=50, null=True)
+  category=models.ManyToManyField(Category)
   slug=models.SlugField(default="",blank=True,null=False,db_index=True,unique=True)
   
-  def save(self,*args,**kwarg):
-    self.slug=slugify(self.name)
-    return super().save(*args,**kwarg)
+  # def save(self,*args,**kwarg):
+  #   self.slug=slugify(self.name)
+  #   return super().save(*args,**kwarg)
 
   def __str__(self):
    return f"name: {self.name} price: {self.price} slug: {self.slug}"
