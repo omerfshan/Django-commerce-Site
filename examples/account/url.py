0e95686652b83e34de2c5eba2a1fc7681ea45697
sahan <101819553+omerfshan@users.noreply.github.com>
2024-12-19 20:50:12 +0300

    update
    Login page and register page updated

--- examples/account/url.py
+++ examples/account/url.py
@@ -4,9 +4,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 urlpatterns = [
-    path('login',views.login,name="login"),
-    path('register',views.register,name="register"),
-    path('logout',views.login,name="logout"),
+    path('login',views.login_request,name="login"),
+    path('register',views.register_request,name="register"),
+    path('logout',views.logout_request,name="logout"),
 
    
 ] 
--- examples/account/views.py
+++ examples/account/views.py
@@ -1,7 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
+def login_request(request):
+    if request.user.is_authenticated:
+        return redirect("index")
+    
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request,username=username,password=password)
+        print(user)
+        print(type(user))
+        if user is not None:
+            login(request,user)
+            return redirect("index")
+        else:
+            return render(request,"account/login.html",{
+           "error":"usename veya parola yanlış"
+        })
+    else:
+        return render(request,"account/login.html",
+           
+        )
+def register_request(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        repassword=request.POST['password2']
+        email=request.POST['email']
+    
+        if password==repassword:
+            if User.objects.filter(username=username).exists():
+                return render(request,"account/register.html",{"error":"aynı isimden var"})
+            else:
+                if User.objects.filter(email=email).exists():
+                    return render(request,"account/register.html",{"error":"aynı mailden var"})
+                else:
+                    user= User.objects.create_superuser(username=username,email=email,password=password)
+                    user.save()
+                    return redirect("login")
+        else:
+         return render(request,"account/register.html",{"error":"sifreler ayni"})
 
-def login(request):
+
+    else:
+       return render(request,"account/register.html")
+
+def logout_request(request):
+    logout(request)
     return render(request,"account/login.html")
-def register(request):
-    return render(request,"account/register.html")
-
